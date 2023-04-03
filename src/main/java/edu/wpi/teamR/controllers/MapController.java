8554ae2aa3ba750c1290bfd7afbdbbd6f09568d1
BryceMcKinley <bryce.j.mckinley@gmail.com>
2023-04-03 16:55:02 -0400

    Buttons added, Zoom fixed
    Added all of the rest of the buttons w/ functionality and fixed the zoom/reset feature

--- src/main/java/edu/wpi/teamR/controllers/MapController.java
+++ src/main/java/edu/wpi/teamR/controllers/MapController.java
@@ -1,7 +1,12 @@
 package edu.wpi.teamR.controllers;
 
 import edu.wpi.teamR.Main;
+import edu.wpi.teamR.navigation.Navigation;
+import edu.wpi.teamR.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.net.URL;
 import javafx.animation.Interpolator;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
@@ -9,6 +14,7 @@ import javafx.scene.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 import net.kurobako.gesturefx.*;
 
@@ -17,12 +23,18 @@ public class MapController {
   @FXML MFXButton searchButton;
   @FXML MFXButton floorUpButton;
   @FXML MFXButton floorDownButton;
+  @FXML MFXCheckbox checkbox;
+
+  @FXML MFXTextField startField;
+  @FXML MFXTextField endField;
 
   @FXML MFXButton homeButton;
   @FXML BorderPane borderPane;
   @FXML AnchorPane anchorPane;
 
   @FXML GesturePane gesturePane;
+
+  @FXML Text floorText;
 
   ImageView groundFloorImage =
       new ImageView(Main.class.getResource("images/00_thegroundfloor.png").toExternalForm());
@@ -37,6 +49,14 @@ public class MapController {
   ImageView lowerTwoImage =
       new ImageView(Main.class.getResource("images/00_thelowerlevel2.png").toExternalForm());
 
+  URL groundFloorLink = Main.class.getResource("images/00_thegroundfloor.png");
+  URL firstFloorLink = Main.class.getResource("images/01_thefirstfloor.png");
+  URL secondFloorLink = Main.class.getResource("images/02_thesecondfloor.png");
+  URL thirdFloorLink = Main.class.getResource("images/03_thethirdfloor.png");
+  URL LLOneLink = Main.class.getResource("images/00_thelowerlevel1.png");
+  URL LLTwoLink = Main.class.getResource("images/00_thelowerlevel2.png");
+
+  ImageView imageView;
   int currentFloor = 2;
   ImageView[] floorArray = {
     lowerTwoImage,
@@ -47,24 +67,40 @@ public class MapController {
     thirdFloorImage
   };
 
+  URL[] linkArray = {
+    LLTwoLink, LLOneLink, groundFloorLink, firstFloorLink, secondFloorLink, thirdFloorLink,
+  };
+
+  String[] floorNames = {
+    "Lower Level Two",
+    "Lower Level One",
+    "Ground Floor",
+    "First Floor",
+    "Second Floor",
+    "Third Floor"
+  };
+
   @FXML
   public void initialize() {
-    gesturePane.setContent(groundFloorImage); // set to ground floor
-
-    //    resetButton.setOnMouseClicked(event -> reset());
-    //    searchButton.setOnMouseClicked(event -> search());
+    imageView = new ImageView(linkArray[currentFloor].toExternalForm());
+    gesturePane.setContent(imageView); // set to groundfloor
+    reset();
+    gesturePane.setMinScale(0.25);
+    gesturePane.setMaxScale(2);
+    resetButton.setOnMouseClicked(event -> reset());
+    searchButton.setOnMouseClicked(event -> search());
     floorDownButton.setOnMouseClicked(event -> displayFloorDown());
     floorUpButton.setOnMouseClicked(event -> displayFloorUp());
-    //    homeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    homeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    floorText.setText(floorNames[currentFloor]);
   }
 
   // Reset to original zoom
   public void reset() {
     // zoom to 1x
-    gesturePane.zoomTo(1, 1, new Point2D(42, 42));
-    gesturePane.zoomTo(1, 1, new Point2D(42, 42));
-    // centre on point [42,42]
-    gesturePane.centreOn(new Point2D(42, 42));
+    gesturePane.zoomTo(0.25, 0.25, new Point2D(2500, 1700));
+    gesturePane.zoomTo(0.25, 0.25, new Point2D(2500, 1700));
+    gesturePane.centreOn(new Point2D(2500, 1700));
   }
 
   // zoom into a desired location
@@ -88,33 +124,34 @@ public class MapController {
     create path between nodes on ALL floors
     create/display textual path? (would have to add spot to display)
      */
+    String start = startField.getText();
+    String end = endField.getText();
+    Boolean isAccessible = checkbox.isPressed();
+    System.out.println("X:" + gesturePane.getCurrentX());
+    System.out.println("Y:" + gesturePane.getCurrentY());
+    System.out.println("XScale:" + gesturePane.getCurrentScaleX());
+    System.out.println("YScale:" + gesturePane.getCurrentScaleY());
   }
 
   public void displayFloorUp() {
-    /*TODO
-    combine all maps into one?
-    if(current floor is not the top)
-        animateZoom to next highest floor
-    else
-        make it known to user in some way they cant go farther
-     */
     if (currentFloor < 5) {
       currentFloor++;
-      gesturePane.setContent(floorArray[currentFloor]); // set to ground floor
+      imageView = new ImageView(linkArray[currentFloor].toExternalForm());
+      gesturePane.setContent(imageView);
+      // gesturePane.setContent(floorArray[currentFloor]); // set to ground floor
+      floorText.setText(floorNames[currentFloor]);
+      reset();
     }
   }
 
   public void displayFloorDown() {
-    /*TODO
-    combine all maps into one?
-    if(current floor is not the top)
-        animateZoom to next highest floor
-    else
-        make it known to user in some way they cant go farther
-     */
     if (currentFloor > 0) {
       currentFloor--;
-      gesturePane.setContent(floorArray[currentFloor]); // set to ground floor
+      imageView = new ImageView(linkArray[currentFloor].toExternalForm());
+      gesturePane.setContent(imageView);
+      // gesturePane.setContent(floorArray[currentFloor]); // set to ground floor
+      floorText.setText(floorNames[currentFloor]);
+      reset();
     }
   }
 }
