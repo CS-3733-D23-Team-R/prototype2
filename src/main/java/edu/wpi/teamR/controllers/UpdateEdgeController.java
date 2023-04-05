75dab20a72b7a60add6a57c923d31b0df0b2e666
jp3wi <jpwilusz@wpi.edu>
2023-04-05 17:50:12 -0400

    Set up framework for editing cells in the edge table.

--- src/main/java/edu/wpi/teamR/controllers/UpdateEdgeController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateEdgeController.java
@@ -3,6 +3,8 @@ package edu.wpi.teamR.controllers;
 import edu.wpi.teamR.database.Node;
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
+import edu.wpi.teamR.database.Edge;
+import edu.wpi.teamR.database.EdgeDAO;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -10,20 +12,54 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UpdateEdgeController{
     @FXML
     MFXButton backButton;
     @FXML
-    TableView nodesTable;
-    @FXML TableColumn startNodeColumn;
-    @FXML TableColumn endNodeColumn;
+    TableView<Edge> nodesTable;
+    @FXML TableColumn<Edge, Integer> startNodeColumn;
+    @FXML TableColumn<Edge, Integer> endNodeColumn;
+
+    static EdgeDAO eDAO;
 
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException, ClassNotFoundException {
+        eDAO = EdgeDAO.createInstance("teamr", "teamr150", "edge", "prototype2", "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb");
+        ObservableList<Edge> edgeList = FXCollections.observableArrayList(EdgeDAO.getInstance().getEdges());
         backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
+        nodesTable.setItems(edgeList);
+        startNodeColumn.setCellValueFactory(new PropertyValueFactory<>("startNode"));
+        endNodeColumn.setCellValueFactory(new PropertyValueFactory<>("endNode"));
+        nodesTable.getColumns().setAll(startNodeColumn, endNodeColumn);
 
+        startNodeColumn.setOnEditCommit(event -> {
+            Edge edge = event.getRowValue();
+            Integer oldStart = edge.getStartNode();
+            edge.setStartNode(event.getNewValue());
+            try {
+                eDAO.deleteConnectingEdge(oldStart, edge.getEndNode());
+                eDAO.addEdge(edge.getStartNode(), edge.getEndNode());
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        endNodeColumn.setOnEditCommit(event -> {
+            Edge edge = event.getRowValue();
+            Integer oldEnd = edge.getEndNode();
+            edge.setStartNode(event.getNewValue());
+            try {
+                eDAO.deleteConnectingEdge(edge.getStartNode(), oldEnd);
+                eDAO.addEdge(edge.getStartNode(), edge.getEndNode());
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
