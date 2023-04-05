112afe0e8aa24f41eae9519ca85bafc86d08d2b3
nathanielitty <nitty@wpi.edu>
2023-04-05 15:16:00 -0400

    finished moves table page

--- src/main/java/edu/wpi/teamR/controllers/UpdateMoveController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateMoveController.java
@@ -1,6 +1,7 @@
 package edu.wpi.teamR.controllers;
 
-import edu.wpi.teamR.database.Node;
+import edu.wpi.teamR.database.Move;
+import edu.wpi.teamR.database.MoveDAO;
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -8,24 +9,77 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
+import javafx.util.converter.DateStringConverter;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class UpdateMoveController{
+    MoveDAO dao;
     @FXML
     MFXButton backButton;
     @FXML
-    TableView nodesTable;
+    TableView<Move> nodesTable;
     @FXML
-    TableColumn nodeIDColumn;
-    @FXML TableColumn nameColumn;
-    @FXML TableColumn dateColumn;
+    TableColumn<Move, Integer> nodeIDColumn;
+    @FXML TableColumn<Move, String> nameColumn;
+    @FXML TableColumn<Move, Date> dateColumn;
+    @FXML TableColumn<Move, Void> deleteColumn;
 
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException, ClassNotFoundException {
         backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
+        setTableColumns();
+    }
 
+    public void setTableColumns() throws SQLException, ClassNotFoundException {
+        dao = MoveDAO.createInstance("teamr", "teamr150", "move",
+                "prototype2", "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb");
+
+        ArrayList<Move> moveList = dao.getMoves();
+        ObservableList<Move> tableData = FXCollections.observableArrayList(moveList);
+        nodesTable.setItems(tableData);
+
+        nodeIDColumn.setCellValueFactory(new PropertyValueFactory<>("nodeID"));
+        nodeIDColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("longName"));
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("moveDate"));
+        dateColumn.setCellFactory(TextFieldTableCell.forTableColumn(new DateStringConverter()));
+
+        deleteColumn.setCellFactory(column -> new TableCell<>() {
+            private final MFXButton deleteButton = new MFXButton("Delete");
+            {
+                deleteButton.setOnAction(event -> {
+                    Move move = getTableView().getItems().get(getIndex());
+                    try {
+                        dao.deleteMove(move.getNodeID(), move.getLongName(), move.getMoveDate());
+                    } catch (SQLException | ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    getTableView().getItems().remove(move);
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(deleteButton);
+                }
+            }
+        });
     }
 }
