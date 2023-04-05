f70ff38fbbdfd7fd6ed924928fab471417cfc9ef
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-04-04 20:56:09 -0400

    Created UpdateEdgeController, UpdateMoveController, UpdateNodesController, UpdateLocationNameController

--- src/main/java/edu/wpi/teamR/controllers/UpdateEdgeController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateEdgeController.java
@@ -1,42 +1,16 @@
 package edu.wpi.teamR.controllers;
 
-import edu.wpi.teamR.database.Node;
-import edu.wpi.teamR.navigation.Navigation;
-import edu.wpi.teamR.navigation.Screen;
-import edu.wpi.teamR.database.Edge;
-import edu.wpi.teamR.database.EdgeDAO;
-import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
+public class UpdateEdgeController extends Application {
 
-public class UpdateEdgeController{
-    @FXML
-    MFXButton backButton;
-    @FXML
-    TableView<Edge> nodesTable;
-    @FXML TableColumn<Edge, Integer> startNodeColumn;
-    @FXML TableColumn<Edge, Integer> endNodeColumn;
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-    static EdgeDAO eDAO;
+    @Override
+    public void start(Stage primaryStage) {
 
-
-    @FXML
-    public void initialize() throws SQLException, ClassNotFoundException {
-        eDAO = EdgeDAO.createInstance("teamr", "teamr150", "edge", "prototype2", "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb");
-        ObservableList<Edge> edgeList = FXCollections.observableArrayList(EdgeDAO.getInstance().getEdges());
-        backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
-        nodesTable.setItems(edgeList);
-        startNodeColumn.setCellValueFactory(new PropertyValueFactory<>("startNode"));
-        endNodeColumn.setCellValueFactory(new PropertyValueFactory<>("endNode"));
-        nodesTable.getColumns().setAll(startNodeColumn, endNodeColumn);
     }
 }
--- src/main/java/edu/wpi/teamR/controllers/UpdateNodesController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateNodesController.java
@@ -8,114 +8,32 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.util.converter.IntegerStringConverter;
-
-import java.sql.SQLException;
+import javax.swing.table.TableColumn;
 import java.util.ArrayList;
 
 public class UpdateNodesController {
 
-  NodeDAO dao;
+  static NodeDAO dao;
   @FXML MFXButton backButton;
-  @FXML TableView<Node> nodesTable;
-  @FXML TableColumn<Node, Integer> nodeIDColumn;
-  @FXML TableColumn<Node, Integer> xCoordinateColumn;
-  @FXML TableColumn<Node, Integer> yCoordinateColumn;
-  @FXML TableColumn<Node, String> floorColumn;
-  @FXML TableColumn<Node, String> buildingColumn;
-  @FXML TableColumn<Node, Void> deleteColumn;
+  @FXML TableView nodesTable;
+  @FXML TableColumn nodeIDColumn;
+  @FXML TableColumn locationNameColumn;
+  @FXML TableColumn startNodeColumn;
+  @FXML TableColumn endNodeColumn;
+
+  private ObservableList<Node> nodeTypeList =
+          FXCollections.observableArrayList()
 
   @FXML
-  public void initialize() throws SQLException, ClassNotFoundException {
+  public void initialize() {
     backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
-    setTableColumns();
+    nodeIDColumn
 
-    xCoordinateColumn.setOnEditCommit(event -> {
-      Node node = event.getRowValue();
-      node.setxCoord(event.getNewValue());
-      try {
-        dao.modifyNodeByID(node.getNodeID(), event.getNewValue(), node.getYCoord(), node.getFloorNum(), node.getBuilding());
-      } catch (SQLException | ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-    });
-    yCoordinateColumn.setOnEditCommit(event -> {
-      Node node = event.getRowValue();
-      node.setyCoord(event.getNewValue());
-      try {
-        dao.modifyNodeByID(node.getNodeID(), node.getXCoord(), event.getNewValue(), node.getFloorNum(), node.getBuilding());
-      } catch (SQLException | ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-    });
-    floorColumn.setOnEditCommit(event -> {
-      Node node = event.getRowValue();
-      node.setFloorNum(event.getNewValue());
-      try {
-        dao.modifyNodeByID(node.getNodeID(), node.getXCoord(), node.getYCoord(), event.getNewValue(), node.getBuilding());
-      } catch (SQLException | ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-    });
-    buildingColumn.setOnEditCommit(event -> {
-      Node node = event.getRowValue();
-      node.setBuilding(event.getNewValue());
-      try {
-        dao.modifyNodeByID(node.getNodeID(), node.getXCoord(), node.getYCoord(), node.getFloorNum(), event.getNewValue());
-      } catch (SQLException | ClassNotFoundException e) {
-        throw new RuntimeException(e);
-      }
-    });
   }
-
-  public void setTableColumns() throws SQLException, ClassNotFoundException {
-    dao = NodeDAO.createInstance("teamr", "teamr150", "node",
-            "prototype2", "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb");
-
-    ArrayList<Node> nodeList = dao.getNodes();
-    ObservableList<Node> tableData = FXCollections.observableArrayList(nodeList);
-    nodesTable.setItems(tableData);
-
-    nodeIDColumn.setCellValueFactory(new PropertyValueFactory<>("nodeID"));
-    nodeIDColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-    xCoordinateColumn.setCellValueFactory(new PropertyValueFactory<>("xCoord"));
-    xCoordinateColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-    yCoordinateColumn.setCellValueFactory(new PropertyValueFactory<>("yCoord"));
-    yCoordinateColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-    floorColumn.setCellValueFactory(new PropertyValueFactory<>("floorNum"));
-    floorColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-    buildingColumn.setCellValueFactory(new PropertyValueFactory<>("building"));
-    buildingColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-    deleteColumn.setCellFactory(column -> new TableCell<>() {
-      private final MFXButton deleteButton = new MFXButton("Delete");
-      {
-        deleteButton.setOnAction(event -> {
-          Node node = getTableView().getItems().get(getIndex());
-          try {
-            dao.deleteNodes(node.getNodeID(), node.getXCoord(), node.getYCoord(), node.getFloorNum(), node.getBuilding());
-          } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-          }
-          getTableView().getItems().remove(node);
-        });
-      }
-
-      @Override
-      protected void updateItem(Void item, boolean empty) {
-        super.updateItem(item, empty);
-
-        if (empty) {
-          setGraphic(null);
-        } else {
-          setGraphic(deleteButton);
-        }
-      }
-    });
+  @FXML
+  public ArrayList<Node> getNodes(){
+    dao = NodeDAO.getInstance();
+    return dao.getNodes();
   }
 }
