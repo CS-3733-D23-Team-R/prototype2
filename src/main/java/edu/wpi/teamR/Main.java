e6f9c76f979834d0f6079fd012444e6d99cd3a6a
Alton Miles <altonmiles@autoreg-5289524.dyn.wpi.edu>
2023-04-04 20:49:14 -0400

    Added stuff to main to make things work

--- src/main/java/edu/wpi/teamR/Main.java
+++ src/main/java/edu/wpi/teamR/Main.java
@@ -1,11 +1,26 @@
 package edu.wpi.teamR;
 
+import edu.wpi.teamR.database.*;
+
+import java.sql.SQLException;
+
 public class Main {
+  public Main() throws SQLException, ClassNotFoundException {
+  }
 
   public static void main(String[] args) {
     App.launch(App.class, args);
   }
-
+  String username = "teamr";
+  String password = "teamr150";
+  String schemaName = "prototype2";
+  String url = "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb";
+  NodeDAO nodeDAO = NodeDAO.createInstance(username, password, "node", schemaName, url);
+  EdgeDAO edgeDAO = EdgeDAO.createInstance(username, password, "edge", schemaName, url);
+  MoveDAO moveDAO = MoveDAO.createInstance(username, password, "move", schemaName, url);
+  LocationNameDAO locationNameDAO = LocationNameDAO.createInstance(username, password, "locationname", schemaName, url);
+  FoodRequestDAO foodRequestDAO = FoodRequestDAO.createInstance(username, password, "foodRequestview", schemaName, url);
+  FurnitureRequestDAO furnitureRequestDAO = FurnitureRequestDAO.createInstance(username, password, "furnitureRequestview", schemaName, url);
   // shortcut: psvm
 
 }
