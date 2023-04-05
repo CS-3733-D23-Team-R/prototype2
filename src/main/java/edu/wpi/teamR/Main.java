cf0cd7dbc56b9da5c494799f038470987baad640
BryceMcKinley <bryce.j.mckinley@gmail.com>
2023-04-04 21:48:46 -0400

    connected service requests to DOA
    Each service request is now saved to the DOA with an ID which is unique across all types of requests

--- src/main/java/edu/wpi/teamR/Main.java
+++ src/main/java/edu/wpi/teamR/Main.java
@@ -8,19 +8,18 @@ public class Main {
   public Main() throws SQLException, ClassNotFoundException {
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    String username = "teamr";
+    String password = "teamr150";
+    String schemaName = "prototype2";
+    String url = "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb";
+    NodeDAO nodeDAO = NodeDAO.createInstance(username, password, "node", schemaName, url);
+    EdgeDAO edgeDAO = EdgeDAO.createInstance(username, password, "edge", schemaName, url);
+    MoveDAO moveDAO = MoveDAO.createInstance(username, password, "move", schemaName, url);
+    LocationNameDAO locationNameDAO = LocationNameDAO.createInstance(username, password, "locationname", schemaName, url);
+    FoodRequestDAO foodRequestDAO = FoodRequestDAO.createInstance(username, password, "foodRequestview", schemaName, url);
+    FurnitureRequestDAO furnitureRequestDAO = FurnitureRequestDAO.createInstance(username, password, "furnitureRequestview", schemaName, url);
     App.launch(App.class, args);
+    // shortcut: psvm
   }
-  String username = "teamr";
-  String password = "teamr150";
-  String schemaName = "prototype2";
-  String url = "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb";
-  NodeDAO nodeDAO = NodeDAO.createInstance(username, password, "node", schemaName, url);
-  EdgeDAO edgeDAO = EdgeDAO.createInstance(username, password, "edge", schemaName, url);
-  MoveDAO moveDAO = MoveDAO.createInstance(username, password, "move", schemaName, url);
-  LocationNameDAO locationNameDAO = LocationNameDAO.createInstance(username, password, "locationname", schemaName, url);
-  FoodRequestDAO foodRequestDAO = FoodRequestDAO.createInstance(username, password, "foodRequestview", schemaName, url);
-  FurnitureRequestDAO furnitureRequestDAO = FurnitureRequestDAO.createInstance(username, password, "furnitureRequestview", schemaName, url);
-  // shortcut: psvm
-
 }
