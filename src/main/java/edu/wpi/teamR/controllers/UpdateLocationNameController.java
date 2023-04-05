785b42abb08d30651080b684754a1f0a5199b9ab
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-04-05 14:43:21 -0400

    Updated UpdateLocationName Controller & Page
    there is an exception occur when trying to open the page in running

--- src/main/java/edu/wpi/teamR/controllers/UpdateLocationNameController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateLocationNameController.java
@@ -1,6 +1,9 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.database.LocationName;
+import edu.wpi.teamR.database.LocationNameDAO;
 import edu.wpi.teamR.database.Node;
+import edu.wpi.teamR.database.NodeDAO;
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -8,23 +11,93 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UpdateLocationNameController{
     @FXML
     MFXButton backButton;
     @FXML
-    TableView nodesTable;
-    @FXML TableColumn longNameColumn;
-    @FXML TableColumn shortNameColumn;
-    @FXML TableColumn nodeTypeColumn;
-
+    TableView<LocationName> nodesTable;
+    @FXML TableColumn<LocationName, String> longNameColumn;
+    @FXML TableColumn<LocationName, String> shortNameColumn;
+    @FXML TableColumn<LocationName, String> nodeTypeColumn;
+    @FXML TableColumn<LocationName, Void> deleteColumn;
+    LocationNameDAO dao;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException, ClassNotFoundException {
+        this.getLocationNames();
         backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
+
+        longNameColumn.setOnEditCommit(event -> {
+            LocationName locationName = event.getRowValue();
+            locationName.setLongName(event.getNewValue());
+        });
+        shortNameColumn.setOnEditCommit(event -> {
+            LocationName locationName = event.getRowValue();
+            locationName.setShortName(event.getNewValue());
+        });
+        nodeTypeColumn.setOnEditCommit(event -> {
+            LocationName locationName = event.getRowValue();
+            locationName.setNodeType(event.getNewValue());
+        });
+    }
+
+    @FXML
+    public void getLocationNames() throws SQLException, ClassNotFoundException {
+        dao = LocationNameDAO.createInstance("teamr", "teamr150", "node",
+                "prototype2", "jdbc:postgresql://database.cs.wpi.edu:5432/teamrdb");
+
+        ArrayList<LocationName> lNList = dao.getLocationNames();
+
+        ObservableList<LocationName> tableData =
+                FXCollections.observableArrayList(lNList);
+
+        nodesTable.setItems(tableData);
+
+        longNameColumn.setCellValueFactory(new PropertyValueFactory<>("Long Name"));
+        longNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        shortNameColumn.setCellValueFactory(new PropertyValueFactory<>("Short Name"));
+        shortNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        nodeTypeColumn.setCellValueFactory(new PropertyValueFactory<>("Node Type"));
+        nodeTypeColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        deleteColumn.setCellFactory(column -> new TableCell<>() {
+            private final MFXButton deleteButton = new MFXButton("Delete");
+            {
+                deleteButton.setOnAction(event -> {
+                    LocationName locationName = getTableView().getItems().get(getIndex());
+                    try {
+                        dao.deleteLocationNames(locationName.getLongName(), locationName.getShortName(), locationName.getNodeType());
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    getTableView().getItems().remove(locationName);
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(deleteButton);
+                }
+            }
+        });
 
     }
 }
