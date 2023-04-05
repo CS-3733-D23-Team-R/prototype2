9341ba366d4ebdd38317307a54680b8d3e8eab44
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2023-04-04 20:56:09 -0400

    Created UpdateEdgeController, UpdateMoveController, UpdateNodesController, UpdateLocationNameController

---
+++ src/main/java/edu/wpi/teamR/controllers/UpdateEdgeController.java
@@ -0,0 +1,16 @@
+package edu.wpi.teamR.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UpdateEdgeController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/main/java/edu/wpi/teamR/controllers/UpdateNodesController.java
+++ src/main/java/edu/wpi/teamR/controllers/UpdateNodesController.java
@@ -1,16 +1,39 @@
 package edu.wpi.teamR.controllers;
 
+import edu.wpi.teamR.database.Node;
+import edu.wpi.teamR.database.NodeDAO;
 import edu.wpi.teamR.navigation.Navigation;
 import edu.wpi.teamR.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javax.swing.table.TableColumn;
+import java.util.ArrayList;
 
 public class UpdateNodesController {
 
+  static NodeDAO dao;
   @FXML MFXButton backButton;
+  @FXML TableView nodesTable;
+  @FXML TableColumn nodeIDColumn;
+  @FXML TableColumn locationNameColumn;
+  @FXML TableColumn startNodeColumn;
+  @FXML TableColumn endNodeColumn;
+
+  private ObservableList<Node> nodeTypeList =
+          FXCollections.observableArrayList()
 
   @FXML
   public void initialize() {
     backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.EMPLOYEE));
+    nodeIDColumn
+
+  }
+  @FXML
+  public ArrayList<Node> getNodes(){
+    dao = NodeDAO.getInstance();
+    return dao.getNodes();
   }
 }
